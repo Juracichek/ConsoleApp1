e6fd7108484b8ab24deb9a7796338b3ceed2bd89
Juracichek <jury.a.ma@gmail.com>
2021-12-01 23:06:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1.Tests/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp1.Tests
+{
+    public class newClassTests
+    {
+        //Equal
+        [Fact]
+        public void now_year()
+        {
+            int currentYear = 2021;
+            int birthYear = 2002;
+            int expected = 19;
+            Program program = new Program();
+            Assert.Equal(expected, program.years(currentYear, birthYear));
+        }
+
+        [Fact]
+        public void grade_for_semester()
+        {
+            int number = 5;
+            string expected = "Отлично";
+            Program program = new Program();
+            Assert.Equal(expected, program.grade(number));
+        }
+
+        [Fact]
+        public void exponentiation()
+        {
+            int a = 2;
+            int expected = 8;
+            Program program = new Program();
+            Assert.Equal(expected, program.exp(a));
+        }
+
+        // True
+        [Fact]
+        public void comp()
+        {
+            int a = 5;
+            int b = 7;
+            bool expected = true;
+            Program program = new Program();
+            Assert.True(expected, program.comparison(a, b));
+        }
+
+        [Fact]
+        public void sNull()
+        {
+            string s1 = "";
+            string s2 = "hello";
+            bool expected = true;
+            Program program = new Program();
+            Assert.True(expected, program.str_null(s1, s2));
+        }
+
+        [Fact]
+        public void even()
+        {
+            int number = 46;
+            bool expected = true;
+            Program program = new Program();
+            Assert.True(expected, program.even_number(number));
+        }
+
+        //Throws<Exception>
+        [Fact]
+        public void height_human_less_null()
+        {
+            Program program = new Program();
+            Assert.Throws<Exception>(() => program.height_human(-6));
+        }
+
+        [Fact]
+        public void height_human_null()
+        {
+            Program program = new Program();
+            Assert.Throws<Exception>(() => program.height_Human(0));
+        }
+
+        [Fact]
+        public void height_human_above_210sm()
+        {
+            Program program = new Program();
+            Assert.Throws<Exception>(() => program.heightHuman(231));
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //Assert.Equal
+        public int years(int currentYear, int birthYear)
+        {
+            return currentYear - birthYear;
+        }
+
+        public string grade(int number)
+        {
+            if (number == 5) return "Отлично";
+            else if (number == 4) return "Хорошо";
+            else if (number == 3) return "Удовлетворительно";
+            else if (number == 2) return "Неудовлетворительно";
+            else if (number == 1) return "Единица";
+            else return "Такой оценки нет!";
+        }
+
+        public int exp(int a)
+        {
+            return a * a * a;
+        }
+
+        // Assert.True
+        public string comparison(int a, int b)
+        {
+            if(a > b) return "a больше b";
+            else return "b больше a";
+        }
+
+        public string str_null(string s1, string s2)
+        {
+            if (s1 == null) return "s1 - пустая";
+            else return "s2 - пустя";
+        }
+
+        public string even_number(int number)
+        {
+            if (number % 2 == 0) return "Число чётное";
+            else return "Число не чётное";
+        }
+
+        //Assert.Throws<Exception>
+        public int height_human(int height)
+        {
+            if(height < 0)
+            {
+                throw new Exception();
+            }
+            return height;
+        }
+
+        public int height_Human(int height)
+        {
+            if (height == 0)
+            {
+                throw new Exception();
+            }
+            return height;
+        }
+
+        public int heightHuman(int height)
+        {
+            if (height > 210)
+            {
+                throw new Exception();
+            }
+            return height;
+        }
+    }
+}
